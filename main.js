d45011a8f69c2ce81a5e31886a77dfe1ca5b6e66
Xondamir0 <alimjanovxondamir01@gmail.com>
2025-01-15 08:37:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+
+ 
+// const ism = prompt("Ismingizni kiriting")
+
+// console.log(ism.split("").reverse("").join(""));
+
+
+// const fruits = [""]
+
+// console.log(fruits);
+
+// alert("")
+
+// confirm = (``)
+
+// if 
+
+
+
+
+
+
+
+let array = ["Olma" , "Banan"  , "Gilos" , "Shaftoli"];
+
+
+console.log("Boshlanishida arryni uzunligi:", array.length);
+alert("Boshlanishida arryni uzunligi: " + array.length);
+
+
+if (confirm("Sizni arrayingizdan ma'lumotni olib tashalmoqchimiz")) {
+
+    array.pop();
+    alert("Arrayingiz uzunligi: " + array.length);
+    console.log("Arrayingiz uzunligi:", array);
+} else {
+
+    alert("Arrayingiz uzunligi: " + array.length);
+    console.log("Arrayingiz uzunligi:", array);
+}
